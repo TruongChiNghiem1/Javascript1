1abfd9a56384bc0252e7b46ce873e3ce7230148c
TruongChiNghiem1 <nghiem0859774418@gmail.com>
2022-11-01 20:00:39 +0700

    nghiem push

---
+++ branch.js
@@ -0,0 +1,15 @@
+console.log(filter1(arr1, "stuff"))
+
+arr2 = [
+    {id: 1, item: 'apple', type: 'fruit'},
+    {id: 2, item: 'banana', type: 'fruit'},
+    {id: 3, item: 'pen', type: 'stuff'},
+    {id: 4, item: 'phone', type: 'stuff'}
+    ]
+
+    arr3 = [
+        {id: 1, item: 'apple', type: 'fruit'},
+        {id: 2, item: 'banana', type: 'fruit'},
+        {id: 3, item: 'pen', type: 'stuff'},
+        {id: 4, item: 'phone', type: 'stuff'}
+        ]
